c5bffe37076fbff2f1b1471cb1cb63875e45bf5c
Mark Gowen <mark.gowen78@gmail.com>
2022-11-29 15:59:46 -0800

    commit tree a76b2009c34fe611f88ba44a1734544612f10a28

--- src/components/Sushi.js
+++ src/components/Sushi.js
@@ -1,20 +1,29 @@
 import React from "react";
 
-function Sushi(props) {
+function Sushi({ sushi, onEatenSushi }) {
+  const { name, img_url, price, eaten } = sushi;
+
+  function handleSushiClick() {
+    if (!eaten) {
+      onEatenSushi(sushi);
+    } else {
+      return null;
+    }
+  }
+
   return (
     <div className="sushi">
-      <div className="plate" onClick={/* Give me a callback! */ null}>
-        {/* Tell me if this sushi has been eaten! */}
-        {false ? null : (
+      <div className="plate" onClick={handleSushiClick}>
+        {eaten ? null : (
           <img
-            src={/* Give me an image source! */ null}
-            alt={/* Give me a name! */ "Sushi"}
+            src={img_url}
+            alt={name}
             width="100%"
           />
         )}
       </div>
       <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
+        {name} - ${price}
       </h4>
     </div>
   );
